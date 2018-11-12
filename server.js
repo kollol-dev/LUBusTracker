63f05055ee4e13ccf982277658bebbe8ec16658e
Kollol Chakraborty <kollol.work@gmail.com>
2018-11-12 11:39:22 +0600

    Mongo Auth Update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const router = express.Router();
 mongoose.Promise = global.Promise;
 
 // Connecting to MongoDB
-mongoose.connect('mongodb://kollol:turnover121@ds259109.mlab.com:59109/gps', {
+mongoose.connect('mongodb://Kollol:turnover121@ds259109.mlab.com:59109/gps', {
     useNewUrlParser: true
 })
     .then(() => console.log('Connected to MongoDB ...'))
